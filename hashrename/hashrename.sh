73089f215413b08da199102e869e8627855d05a0
cmsong <dlcmaldmlzldxkdlrj@gmail.com>
2023-05-17 00:31:15 +0900

    feat(hashrename): improve error handling, usage, option detecting

--- hashrename/hashrename.sh
+++ hashrename/hashrename.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-# Check if the first argument is --dry.
-if [[ $1 == "--dry" ]]
+DRY_RUN=0
+
+while getopts ":d" opt; do
+  case ${opt} in
+    d )
+      DRY_RUN=1
+      ;;
+    \? )
+      echo "Invalid option: $OPTARG" 1>&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+# Check if any arguments were provided.
+if [ $# -eq 0 ]
 then
-    dry_run=true
-    shift 1
-else
-    dry_run=false
+    echo "Usage: $0 [-d] file_or_glob_pattern ..."
+    exit 1
 fi
 
 # Process the rest of the arguments as files or glob patterns.
@@ -15,19 +28,45 @@ do
     # Expand the glob pattern or file name.
     for file in $arg
     do
+        # Error handling
+        if [ ! -f "$file" ]; then
+            echo "File $file does not exist."
+            continue
+        fi
+
+        if [ ! -r "$file" ]; then
+            echo "File $file cannot be read."
+            continue
+        fi
+
         # Compute the SHA-256 hash of the file.
         sha256_hash=$(shasum -a 256 "$file" | awk '{print $1}')
 
         # Get the file extension.
-        file_extension="${file##*.}"
+        file_name=$(basename "$file")
+        file_dir=$(dirname "$file")
+        file_extension="${file_name##*.}"
+        file_basename="${file_name%.*}"
+
+        # If the file doesn't have an extension, file_extension will be the same as file_basename.
+        # In that case, we don't want to append a dot after the hash.
+        if [ "$file_extension" = "$file_basename" ]; then
+            new_name="${file_dir}/${sha256_hash}"
+        else
+            new_name="${file_dir}/${sha256_hash}.${file_extension}"
+        fi
+
+        if [ -e "$new_name" ]; then
+            echo "File $new_name already exists."
+            continue
+        fi
 
         # If this is a dry run, print what would be done.
-        if $dry_run
-        then
-            echo "$file -> ${sha256_hash}.$file_extension"
+        if ((DRY_RUN)); then
+            echo "$file -> $new_name"
         else
             # Rename the file to its SHA-256 hash, preserving the file extension.
-            mv -- "$file" "${sha256_hash}.$file_extension"
+            mv -- "$file" "$new_name"
         fi
     done
 done
